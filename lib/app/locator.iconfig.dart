fd2f2aa55f36094405637a145ba9bd524e967469
JM REMEUR <jm@remeur.fr>
2020-05-18 12:45:23 +0200

    update locator

--- lib/app/locator.iconfig.dart
+++ lib/app/locator.iconfig.dart
@@ -6,13 +6,31 @@
 
 import 'package:mpm/services/database/database.dart';
 import 'package:mpm/services/api/auth_api.dart';
+import 'package:mpm/services/third_party_services.dart';
+import 'package:stacked_services/stacked_services.dart';
 import 'package:mpm/services/secure_storage.dart';
 import 'package:get_it/get_it.dart';
 
 void $initGetIt(GetIt g, {String environment}) {
+  final thirdPartyServices = _$ThirdPartyServices();
   g.registerLazySingleton<AuthApi>(() => AuthApi.create());
+  g.registerLazySingleton<DialogService>(
+      () => thirdPartyServices.dialogService);
+  g.registerLazySingleton<NavigationService>(
+      () => thirdPartyServices.navigationService);
+  g.registerLazySingleton<SnackbarService>(
+      () => thirdPartyServices.snackBarService);
 
   //Eager singletons must be registered in the right order
   g.registerSingletonAsync<AppDatabase>(() => AppDatabase.create());
   g.registerSingleton<SecureStorage>(SecureStorage());
+}
+
+class _$ThirdPartyServices extends ThirdPartyServices {
+  @override
+  DialogService get dialogService => DialogService();
+  @override
+  NavigationService get navigationService => NavigationService();
+  @override
+  SnackbarService get snackBarService => SnackbarService();
 }
