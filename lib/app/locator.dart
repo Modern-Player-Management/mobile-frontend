41c4161396746b7c1f8c0e89d291e9abda96b895
JM REMEUR <jm@remeur.fr>
2020-05-18 16:08:48 +0200

    rename get it to locator and export services

--- lib/app/locator.dart
+++ lib/app/locator.dart
@@ -3,11 +3,17 @@ import 'package:injectable/injectable.dart';
 
 import 'package:mpm/app/locator.iconfig.dart';
 
-final getIt = GetIt.instance;
+export 'package:mpm/services/database/database.dart';
+export 'package:mpm/services/api/auth_api.dart';
+export 'package:mpm/services/secure_storage.dart';
+export 'package:mpm/services/third_party_services.dart';
+export 'package:stacked_services/stacked_services.dart';
+
+final locator = GetIt.instance;
 
 @injectableInit
 Future<void> configure() async 
 {
-	$initGetIt(getIt);
-	await getIt.allReady(ignorePendingAsyncCreation: false);
+	$initGetIt(locator);
+	await locator.allReady(ignorePendingAsyncCreation: false);
 }
