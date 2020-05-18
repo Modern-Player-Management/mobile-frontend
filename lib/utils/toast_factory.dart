5e69a146621aff934da0f88286565880cec7a8c0
JM REMEUR <jm@remeur.fr>
2020-05-18 10:54:27 +0200

    utils colors and other

---
+++ lib/utils/toast_factory.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+import 'package:bot_toast/bot_toast.dart';
+
+enum ToastStyle
+{
+	error,
+	warning,
+	success,
+	info
+}
+
+class ToastFactory
+{
+	static void show({
+		@required BuildContext context, 
+		@required String msg, 
+		ToastStyle style = ToastStyle.info,
+		Duration duration = const Duration(milliseconds: 1500)
+	})
+	{
+		IconData icon;
+		Color color;
+
+		switch(style)
+		{
+			case ToastStyle.error: 
+				icon = Icons.error;
+				color = Colors.red.shade800;
+				break;
+			case ToastStyle.warning: 
+				icon = Icons.warning;
+				color = Colors.yellow.shade800;
+				break;
+			case ToastStyle.success: 
+				icon = Icons.info;
+				color = Colors.green.shade800;
+				break;
+			default: 
+				icon = Icons.info;
+				color = Colors.blue.shade800;
+				break;
+		}
+
+		showCustom(
+			context: context,
+			icon: Icon(
+				icon,
+				size: 32,
+				color: Colors.white,
+			),
+			text: Text(
+				msg,
+				style: TextStyle(
+					color: Colors.white,
+					fontSize: 18
+				),
+			),
+			background: color
+		);
+	}
+
+	static void showCustom({
+		@required BuildContext context,
+		@required Widget icon,
+		@required Widget text,
+		@required Color background,
+		Duration duration = const Duration(milliseconds: 1500),
+		Duration animationDuration = const Duration(milliseconds: 300),
+		bool crossPage = false
+	})
+	{
+		BotToast.showCustomNotification(
+			toastBuilder: (_){
+				return Card(
+					color: background,
+					child: ListTile(
+						leading: icon,
+						title: text
+					),
+				);
+			},
+			duration: duration,
+			animationDuration: animationDuration,
+			crossPage: crossPage,
+		);
+	}
+}
