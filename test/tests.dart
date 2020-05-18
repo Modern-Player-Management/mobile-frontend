6fe4ced30ce7f126b0c9f91f61a4016786816233
JM REMEUR <jm@remeur.fr>
2020-05-18 15:05:28 +0200

    add api tests

--- test/tests.dart
+++ test/tests.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sqflite_ffi_test/sqflite_ffi_test.dart';
 
+import 'services/api/api_test.dart';
 import 'services/database/database_test.dart';
 
 void main() 
@@ -9,4 +10,5 @@ void main()
 	sqfliteFfiTestInit();
 
 	database_tests();
+	api_tests();
 }
