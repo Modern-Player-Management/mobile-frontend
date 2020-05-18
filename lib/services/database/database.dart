09a7c57ea6ece7dae9b0991fefa401eb4be5faa6
JM REMEUR <jm@remeur.fr>
2020-05-18 12:11:47 +0200

    create database

---
+++ lib/services/database/database.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+
+import 'package:floor/floor.dart';
+import 'package:injectable/injectable.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart' as sqflite;
+
+import 'package:mpm/services/database/models/models.dart';
+export 'package:mpm/services/database/models/models.dart';
+
+import 'package:mpm/services/database/daos/daos.dart';
+export 'package:mpm/services/database/daos/daos.dart';
+
+part 'database.g.dart';
+
+@Database(entities: [
+	Team,
+	Event,
+], version: 1)
+@singleton
+abstract class AppDatabase extends FloorDatabase 
+{
+	TeamDao get teamDao;
+	EventDao get eventDao;
+
+	@factoryMethod
+	static Future<AppDatabase> create() async 
+	{
+		return await $FloorAppDatabase.databaseBuilder('database.db').build();
+	}
+}
