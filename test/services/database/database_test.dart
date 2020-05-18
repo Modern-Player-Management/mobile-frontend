61372dcd9629016f7f61b22770c2521532680256
JM REMEUR <jm@remeur.fr>
2020-05-18 15:05:18 +0200

    one complex test with on team of two events deleting the team, foreign key should delete cascade

--- test/services/database/database_test.dart
+++ test/services/database/database_test.dart
@@ -12,21 +12,60 @@ void database_tests() async
 		db = await $FloorAppDatabase.inMemoryDatabaseBuilder().build();
 	});
 
-	group('Database team tests', (){
-		test('Insert one team', insert_team);
-		test('Find one team', find_team);
-		test('Update team', update_team);
-		test('Delete team', delete_team);
+	group('simple database team tests', (){
+		test('insert one team', insert_team);
+		test('find one team', find_team);
+		test('update team', update_team);
+		test('delete team', delete_team);
 	});
 
-	group('Database event tests', (){
-		test('Insert one event', insert_event);
-		test('Find one event', find_event);
-		test('Update event', update_event);
-		test('Delete event', delete_event);
+	group('simple database event tests', (){
+		test('insert one event', insert_event);
+		test('find one event', find_event);
+		test('update event', update_event);
+		test('delete event', delete_event);
+	});
+
+	group('complex database tests', (){
+		test('delete team with two events', delete_team_with_two_events);
 	});
 
 	tearDown((){
 		db.close();
 	});
+}
+
+void delete_team_with_two_events() async
+{
+	final team = Team(
+		id: teamId,
+		user: user,
+		name: "name"
+	);
+
+	final event1 = Event(
+		id: "1",
+		team: teamId,
+		title: "test",
+		description: "test"
+	);
+
+	final event2 = Event(
+		id: "2",
+		team: teamId,
+		title: "test",
+		description: "test"
+	);
+
+	await db.teamDao.insertTeam(team);
+	await db.eventDao.insertEvent(event1);
+	await db.eventDao.insertEvent(event2);
+
+	await db.teamDao.deleteTeam(team);
+
+	final teams = await db.teamDao.getTeams(user).first;
+	expect(teams.length, equals(0));
+
+	final events = await db.eventDao.getEvents(teamId).first;
+	expect(events.length, equals(0));
 }
