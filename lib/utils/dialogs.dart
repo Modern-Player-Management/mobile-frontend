3b19595ef0c96f117d13027b5ecbbebbc4012717
JM REMEUR <jm@remeur.fr>
2020-05-18 10:54:27 +0200

    utils colors and other

---
+++ lib/utils/dialogs.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void showLoadingDialog(BuildContext context, [Function willPop])
+{
+	showDialog(
+		context: context,
+		builder: (_) => WillPopScope(
+			onWillPop: () async {
+				if(willPop != null)
+				{
+					willPop();
+				}
+				return true;
+			},
+			child: Material(
+				type: MaterialType.transparency,
+				child: Center(
+					child: CircularProgressIndicator(),
+				),
+			),
+		)
+	);
+}
+
+Future<bool> showConfirmDialog(BuildContext context, String title, String content) async
+{
+	return await showDialog<bool>(
+		barrierDismissible: false,
+		context: context,
+		builder: (BuildContext context) {
+			return AlertDialog(
+				title: Text(title),
+				content: Text(content),
+				actions: <Widget>[
+					FlatButton(
+						child: new Text("Cancel"),
+						onPressed: () {
+							Navigator.pop(context, false);
+						},
+					),
+					FlatButton(
+						child: new Text("Ok"),
+						onPressed: () {
+							Navigator.pop(context, true);
+						},
+					),
+				],
+			);
+		},
+    );
+}
---
+++ lib/utils/snackbar_factory.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+enum MessageStyle
+{
+	error,
+	warning,
+	success,
+	info
+}
+
+// easy snackbar
+// show snackbar with different theme
+class SnackbarFactory
+{
+	static void show(BuildContext context, String msg, MessageStyle style, {int duration = 2, ScaffoldState scaffoldState})
+	{
+		IconData icon;
+		Color backgroundColor;
+
+		switch(style)
+		{
+			case MessageStyle.error:
+				icon = Icons.info_outline;
+				backgroundColor = Colors.red.shade800;
+				break;
+			case MessageStyle.warning:
+				icon = Icons.info_outline;
+				backgroundColor = Colors.yellow.shade800;
+				break;
+			case MessageStyle.success:
+				icon = Icons.info_outline;
+				backgroundColor = Colors.green.shade800;
+				break;
+			case MessageStyle.info:
+			default:
+				icon = Icons.info_outline;
+				backgroundColor = Colors.blue.shade800;
+		}
+
+		if(scaffoldState == null)
+		{
+			scaffoldState = Scaffold.of(context);
+		}
+
+		scaffoldState.showSnackBar(
+			SnackBar(
+				backgroundColor: backgroundColor,
+				content: Row(
+					children: <Widget>[
+						Padding(
+							padding: const EdgeInsets.only(right: 8.0),
+							child: Icon(icon, size: 30),
+						),
+						Expanded(child: Text(msg))
+					],
+				),
+				duration: Duration(seconds: duration),
+			)
+		);
+	}
+}
