8626b6f4fc9662d0942f3a0ff3d2befca546f183
JM REMEUR <jm@remeur.fr>
2020-05-18 15:04:25 +0200

    api tests

---
+++ test/services/api/auth_api_test.dart
@@ -0,0 +1,35 @@
+
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/testing.dart';
+import 'package:mpm/services/api/auth_api.dart';
+
+void register() async
+{
+	final client = MockClient((req) async {
+		expect(req.url.path, equals("/register"));
+		return http.Response(json.encode({}), 200);
+	});
+
+	AuthApi api = AuthApi.createWith(client);
+
+	final res = await api.register("test", "test@test.com", "test");
+
+	expect(res.isSuccessful, equals(true));
+}
+
+void authenticate() async
+{
+	final client = MockClient((req) async {
+		expect(req.url.path, equals("/authenticate"));
+		return http.Response(json.encode({}), 200);
+	});
+
+	AuthApi api = AuthApi.createWith(client);
+
+	final res = await api.authenticate("test@test.com", "test");
+
+	expect(res.isSuccessful, equals(true));
+}
