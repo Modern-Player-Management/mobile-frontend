95aefc9451c99e112abb3df3052661f60eaf4725
JM REMEUR <jm@remeur.fr>
2020-05-18 14:26:00 +0200

    databases tests for teams and events

---
+++ test/tests.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sqflite_ffi_test/sqflite_ffi_test.dart';
+
+import 'services/database/database_test.dart';
+
+void main() 
+{
+	TestWidgetsFlutterBinding.ensureInitialized();
+	sqfliteFfiTestInit();
+
+	database_tests();
+}
