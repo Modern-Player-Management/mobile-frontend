f354597c1e664bb0b88af5dea7e9cadadc069231
JM REMEUR <jm@remeur.fr>
2020-05-18 12:44:49 +0200

    import third party services from stacked services

---
+++ lib/services/third_party_services.dart
@@ -0,0 +1,15 @@
+import 'package:injectable/injectable.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+@module
+abstract class ThirdPartyServices
+{
+	@lazySingleton
+	NavigationService get navigationService;
+
+	@lazySingleton
+	DialogService get dialogService;
+	
+	@lazySingleton
+	SnackbarService get snackBarService;
+}
