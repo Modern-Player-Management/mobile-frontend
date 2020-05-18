ee1be238abf596c04db81c138c70b9305b4d6864
JM REMEUR <jm@remeur.fr>
2020-05-18 12:13:59 +0200

    update locator

--- lib/app/locator.iconfig.dart
+++ lib/app/locator.iconfig.dart
@@ -4,9 +4,15 @@
 // InjectableConfigGenerator
 // **************************************************************************
 
+import 'package:mpm/services/database/database.dart';
+import 'package:mpm/services/api/auth_api.dart';
 import 'package:mpm/services/secure_storage.dart';
 import 'package:get_it/get_it.dart';
 
 void $initGetIt(GetIt g, {String environment}) {
+  g.registerLazySingleton<AuthApi>(() => AuthApi.create());
+
+  //Eager singletons must be registered in the right order
+  g.registerSingletonAsync<AppDatabase>(() => AppDatabase.create());
   g.registerSingleton<SecureStorage>(SecureStorage());
 }
