39b805233f7781153fd4fd8e6e27d54b23858b97
JM REMEUR <jm@remeur.fr>
2020-05-19 12:09:25 +0200

    added uuid service bump stacked version

--- lib/app/locator.dart
+++ lib/app/locator.dart
@@ -4,10 +4,13 @@ import 'package:injectable/injectable.dart';
 import 'package:mpm/app/locator.iconfig.dart';
 
 export 'package:mpm/services/database/database.dart';
+
 export 'package:mpm/services/api/auth_api.dart';
+export 'package:mpm/services/api/team_api.dart';
+
 export 'package:mpm/services/secure_storage.dart';
-export 'package:mpm/services/third_party_services.dart';
 export 'package:stacked_services/stacked_services.dart';
+export 'package:uuid/uuid.dart';
 
 final locator = GetIt.instance;
 
