30a52c1ec5426cbd1e509fc10b592d32991d2c76
JM REMEUR <jm@remeur.fr>
2020-05-21 17:54:00 +0200

    created a app widget to handle app life cycle and internet status

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import 'package:bot_toast/bot_toast.dart';
+import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
+import 'package:dynamic_theme/dynamic_theme.dart';
 
 import 'package:mpm/app/locator.dart';
 import 'package:mpm/utils/colors.dart';
@@ -14,19 +18,98 @@ void main() async
 	await configure();
 
 	// run app
-	runApp(MaterialApp(
-		title: "Modern Player Management",
-		builder: BotToastInit(),
-		navigatorObservers: [BotToastNavigatorObserver()],
-		theme: ThemeData(
-			primaryColor: ThemeColors.primary,
-			accentColor: ThemeColors.accent,
-			primaryTextTheme: TextTheme(
-				
-			)
-		),
-		initialRoute: Routes.splashViewRoute,
-		onGenerateRoute: Router().onGenerateRoute,
-		navigatorKey: locator<NavigationService>().navigatorKey,
-	));
+	runApp(App());
+}
+
+class App extends ViewModelBuilderWidget<AppModel> with WidgetsBindingObserver
+{
+	@override
+	bool get reactive => false;
+
+	final _model = AppModel();
+
+	@override
+	void didChangeAppLifecycleState(AppLifecycleState state) 
+	{
+		switch(state)
+		{
+			case AppLifecycleState.resumed: 
+				_model.resume();
+				break;
+			case AppLifecycleState.paused:
+				_model.pause();
+				break;
+			case AppLifecycleState.inactive:
+				break;
+			case AppLifecycleState.detached: 
+				break;
+			default: break;
+		}
+	}
+
+  	@override
+	Widget builder(context, model, child) 
+	{
+		return DynamicTheme(
+			defaultBrightness: Brightness.light,
+			data: (brightness) {
+				return ThemeData(
+					primaryColor: ThemeColors.primary,
+					accentColor: ThemeColors.accent,
+				);
+			},
+			themedWidgetBuilder: (context, theme) {
+				return MaterialApp(
+					title: "Modern Player Management",
+					builder: BotToastInit(),
+					navigatorObservers: [BotToastNavigatorObserver()],
+					theme: theme,
+					initialRoute: Routes.splashViewRoute,
+					onGenerateRoute: Router().onGenerateRoute,
+					navigatorKey: locator<NavigationService>().navigatorKey,
+				);
+			},
+		);
+	}
+  
+	@override
+	AppModel viewModelBuilder(context) 
+	{
+		WidgetsBinding.instance.addObserver(this);
+		return _model;
+	}
+}
+
+class AppModel extends BaseViewModel
+{
+	final _connectivity = locator<Connectivity>();
+
+	StreamSubscription _subscription;
+
+	AppModel()
+	{
+		resume();
+	}
+
+	void _onConnectivityChanged(ConnectivityResult result)
+	{
+
+	}
+
+	void pause()
+	{
+		_subscription.cancel();
+	}
+
+	void resume()
+	{
+		_subscription = _connectivity.onConnectivityChanged.listen(_onConnectivityChanged);
+	}
+
+	@override
+	void dispose()
+	{ 
+		pause();
+		super.dispose();
+	}
 }
