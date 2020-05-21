4b648244247f054c6c92cc0c76ca031b86a98dc9
JM REMEUR <jm@remeur.fr>
2020-05-21 16:27:23 +0200

    update locator with new services

--- lib/app/locator.iconfig.dart
+++ lib/app/locator.iconfig.dart
@@ -7,8 +7,10 @@
 import 'package:mpm/services/database/database.dart';
 import 'package:mpm/services/api/auth_api.dart';
 import 'package:mpm/services/third_party_services.dart';
+import 'package:connectivity/connectivity.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:mpm/services/secure_storage.dart';
+import 'package:mpm/services/session.dart';
 import 'package:mpm/services/api/team_api.dart';
 import 'package:mpm/models/managers/team_manager.dart';
 import 'package:uuid/uuid.dart';
@@ -17,10 +19,12 @@ import 'package:get_it/get_it.dart';
 void $initGetIt(GetIt g, {String environment}) {
   final thirdPartyServices = _$ThirdPartyServices();
   g.registerLazySingleton<AuthApi>(() => AuthApi.create());
+  g.registerLazySingleton<Connectivity>(() => thirdPartyServices.connectivity);
   g.registerLazySingleton<DialogService>(
       () => thirdPartyServices.dialogService);
   g.registerLazySingleton<NavigationService>(
       () => thirdPartyServices.navigationService);
+  g.registerLazySingleton<Session>(() => Session());
   g.registerLazySingleton<SnackbarService>(
       () => thirdPartyServices.snackBarService);
   g.registerLazySingleton<TeamApi>(() => TeamApi.create());
