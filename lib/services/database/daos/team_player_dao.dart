a44e003d27ff727fd122ae52bf2a526d4e73b09a
JM REMEUR <jm@remeur.fr>
2020-05-21 14:54:47 +0200

    created team player dao update entity id queries removed useless player queries get players and player from ids or id

---
+++ lib/services/database/daos/team_player_dao.dart
@@ -0,0 +1,31 @@
+import 'package:floor/floor.dart';
+
+import 'package:mpm/services/database/models/team_player.dart';
+
+@dao
+abstract class TeamPlayerDao
+{
+	@Query('select * from teams_players where teamId = :teamId and `delete` = 0')
+	Stream<List<TeamPlayer>> getTeamPlayers(String teamId);
+
+	@Query('select * from teams_players where teamId = :teamId and save = 1 and `delete` = 0')
+	Future<List<TeamPlayer>> getSavedTeamPlayers(String teamId);
+
+	@Query('select * from teams_players where teamId = :teamId and save = 0 and `delete` = 0')
+	Future<List<TeamPlayer>> getUnsavedTeamPlayers(String teamId);
+
+	@Query('select * from teams_players where teamId = :teamId and `delete` = 1')
+	Future<List<TeamPlayer>> getUndeletedTeamPlayers(String teamId);
+
+	@Query('select * from teams_players where teamId = :teamId and playerId = :playerId')
+	Future<TeamPlayer> getTeamPlayer(String teamId, String playerId);
+
+	@insert
+	Future<int> insertTeamPlayer(TeamPlayer teamPlayer);
+
+	@update
+	Future<int> updateTeamPlayer(TeamPlayer teamPlayer);
+
+	@delete
+	Future<int> deleteTeamPlayer(TeamPlayer teamPlayer);
+}
