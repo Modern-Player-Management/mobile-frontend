a0db422a55bbb1794d6bdc2a72947382a94a941f
JM REMEUR <jm@remeur.fr>
2020-05-22 23:34:50 +0200

    home view display teams with manager name

--- lib/ui/views/home/home_view_model.dart
+++ lib/ui/views/home/home_view_model.dart
@@ -6,13 +6,59 @@ import 'package:mpm/app/locator.dart';
 
 class HomeViewModel extends BaseViewModel
 {
-	final BuildContext context;
+	final _teamManager = locator<TeamManager>();
+	final _navigation = locator<NavigationService>();
 
-	TeamManager _teamManager = locator<TeamManager>();
+	final BuildContext context;
 
 	HomeViewModel({
 		@required this.context
 	});
 
-	Stream<List<Team>> get teams => _teamManager.getTeams();
+	Future<void> onRefresh() async
+	{
+		await _teamManager.syncTeams();
+		Future.delayed(Duration(seconds: 2));
+	}
+
+	void createTeam()
+	{
+		_navigation.navigateTo(Routes.createTeamViewRoute);
+	}
+}
+
+class TeamsViewModel extends StreamViewModel<List<Team>>
+{
+	@override
+	Stream<List<Team>> get stream => locator<TeamManager>().getTeams();
+}
+
+class TeamViewModel extends FutureViewModel<void>
+{
+	final _db = locator<AppDatabase>();
+
+	final Team team;
+	final List<Player> players = [];
+	Player manager;
+
+	bool loaded = false;
+
+	TeamViewModel({
+		@required this.team
+	});
+
+	@override
+	Future<void> futureToRun() async
+	{
+		manager = await _db.playerDao.getPlayer(team.managerId);
+
+		var teamPlayers = await _db.teamPlayerDao.getTeamPlayers(team.id);
+		for(var teamPlayer in teamPlayers)
+		{
+			var player = await _db.playerDao.getPlayer(teamPlayer.playerId);
+			players.add(player);
+		}
+
+		loaded = true;
+	}
 }
