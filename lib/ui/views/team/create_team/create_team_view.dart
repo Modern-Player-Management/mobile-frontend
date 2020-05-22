5d40fdb0aef50b5491b05e96e479bbd988a34b69
JM REMEUR <jm@remeur.fr>
2020-05-22 23:40:07 +0200

    WIP : create team view simple file piccker

---
+++ lib/ui/views/team/create_team/create_team_view.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+
+import 'package:stacked/stacked.dart';
+import 'package:stacked_hooks/stacked_hooks.dart';
+
+import 'package:mpm/ui/views/team/create_team/create_team_view_model.dart';
+import 'package:mpm/ui/widgets/button.dart';
+
+class CreateTeamView extends ViewModelBuilderWidget<CreateTeamViewModel>
+{
+	@override
+  	bool get reactive => false;
+
+  	@override
+  	Widget builder(context, model, child)
+	{
+		return Scaffold(
+			appBar: AppBar(
+				title: Text(
+					"Create a team"
+				),
+			),
+			body: Padding(
+				padding: const EdgeInsets.all(8.0),
+				child: Form(
+					key: model.formKey,
+					child: Column(
+						children: <Widget>[
+							_ImagePicker(),
+							SizedBox(height: 8),
+							_NameTextField(),
+						],
+					),
+				),
+			),
+			floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+			floatingActionButton: Button(
+				child: Text(
+					"Create a team"
+				),
+				color: Colors.green,
+				onPressed: model.createTeam,
+			),
+		);
+	}
+  
+	@override
+	CreateTeamViewModel viewModelBuilder(context)
+	{
+		return CreateTeamViewModel();
+	}
+}
+
+class _ImagePicker extends HookViewModelWidget<CreateTeamViewModel>
+{
+	@override
+  	bool get reactive => false;
+
+	@override
+	Widget buildViewModelWidget(context, model) 
+	{
+		return InkWell(
+			child: CircleAvatar(
+				radius: 32,
+				child: Icon(
+					Icons.star
+				),
+			),
+			onTap: model.selectImage,
+		);
+	}
+}
+
+class _NameTextField extends HookViewModelWidget<CreateTeamViewModel>
+{
+	@override
+  	bool get reactive => false;
+
+	@override
+	Widget buildViewModelWidget(context, model) 
+	{
+		return TextFormField(
+			initialValue: model.team.name,
+			decoration: InputDecoration(
+				labelText: "Name",
+				prefixIcon: Icon(
+					Icons.title,
+					size: 32,
+				),
+			),
+			validator: model.descriptionValidator,
+			onChanged: (str) => model.team.name = str,
+			onSaved: (str) => model.team.name = str,
+		);
+	}
+}
+
+class _DescriptionTextField extends HookViewModelWidget<CreateTeamViewModel>
+{
+	@override
+  	bool get reactive => false;
+
+	@override
+	Widget buildViewModelWidget(context, model) 
+	{
+		return TextFormField(
+			initialValue: model.team.description,
+			decoration: InputDecoration(
+				labelText: "Name",
+				prefixIcon: Icon(
+					Icons.title,
+					size: 32,
+				),
+			),
+			validator: model.nameValidator,
+			onChanged: (str) => model.team.description = str,
+			onSaved: (str) => model.team.description = str,
+		);
+	}
+}
---
+++ lib/ui/views/team/create_team/create_team_view_model.dart
@@ -0,0 +1,67 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+import 'package:permission_handler/permission_handler.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:stacked/stacked.dart';
+
+import 'package:mpm/app/locator.dart';
+
+class CreateTeamViewModel extends BaseViewModel
+{
+	final _teamManager = locator<TeamManager>();
+
+	final _minCharacters = 3;
+	final formKey = GlobalKey<FormState>();
+
+	Team team = Team();
+
+	String nameValidator(String str)
+	{
+		if(str.isEmpty || str.length < _minCharacters)
+		{
+			return "You must enter a name with at least 3 characters";
+		}
+
+		return null;
+	}
+
+	String descriptionValidator(String str)
+	{
+		if(str.isEmpty || str.length < _minCharacters)
+		{
+			return "You must enter a description with at least 3 characters";
+		}
+
+		return null;
+	}
+
+	void selectImage() async
+	{
+		if(!await Permission.storage.isGranted)
+		{
+			await Permission.storage.request();
+		}
+
+		if(!await Permission.storage.isGranted)
+		{
+			//todo: display error message
+			return;
+		}
+
+		File file = await FilePicker.getFile(
+			type: FileType.image
+		);
+	}
+
+	void createTeam()
+	{
+		if(formKey.currentState.validate())
+		{
+			formKey.currentState.save();
+			
+			_teamManager.insertTeam(team);
+		}
+	}
+}
