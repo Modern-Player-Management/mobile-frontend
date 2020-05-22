8715f2a5da6d0319552d5b55ad2d9ca029621569
JM REMEUR <jm@remeur.fr>
2020-05-22 19:22:21 +0200

    created file api

---
+++ lib/services/api/file_api.dart
@@ -0,0 +1,53 @@
+import 'dart:io';
+
+import 'package:chopper/chopper.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart' show MultipartFile;
+import 'package:get_it/get_it.dart';
+import 'package:http/io_client.dart';
+import 'package:injectable/injectable.dart';
+
+import 'package:mpm/app/locator.dart';
+import 'package:mpm/services/api/converters/json_serializable_converter.dart';
+import 'package:mpm/utils/utils.dart';
+
+part 'file_api.chopper.dart';
+
+@ChopperApi()
+@lazySingleton
+abstract class FileApi extends ChopperService
+{
+	@factoryMethod
+	static FileApi create() => createWith();
+
+	@factoryMethod
+	static FileApi createWith([http.BaseClient client])
+	{
+		return _$FileApi(ChopperClient(
+			client: IOClient(
+				HttpClient()..connectionTimeout = const Duration(seconds: 4),
+			),
+			baseUrl: serverUrl,
+			converter: JsonSerializableConverter({
+				
+			}),
+			interceptors: [
+				(Request request) {
+					final headers = Map<String, String>.from(request.headers);
+					var token = GetIt.instance<SecureStorage>().token;
+					headers['Authorization'] = 'Bearer $token';
+					return request.copyWith(headers: headers);
+				}
+			]
+		));
+	}
+
+	// Files requests
+
+	@Post(path: "/api/files")
+	@multipart
+	Future<Response<Map<String , String>>> uploadFile(@PartFile() MultipartFile file);
+
+	@Get(path: "/api/files/{fileId}")
+	Future<Response> getFile(@Path() String fileId);
+}
