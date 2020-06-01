a215bfc887abab07a88438af25adfa0cffee646b
JM REMEUR <jm@remeur.fr>
2020-06-01 14:30:30 +0200

    simple team view

--- lib/ui/views/team/team_view.dart
+++ lib/ui/views/team/team_view.dart
@@ -20,12 +20,18 @@ class TeamView extends ViewModelBuilderWidget<TeamViewModel>
   	Widget builder(context, model, child)
 	{
 		return Scaffold(
-			appBar: AppBar(
-				title: Text(
-					"MPM"
-				),
+			body: CustomScrollView(
+				slivers: <Widget>[
+					_AppBar(),
+					_TeamPlayersView()
+				],
 			),
-			body: Container()
+			floatingActionButton: FloatingActionButton(
+				child: Icon(
+					Icons.add
+				),
+				onPressed: model.addPlayer,
+			),
 		);
 	}
   
@@ -34,6 +40,63 @@ class TeamView extends ViewModelBuilderWidget<TeamViewModel>
 	{
 		return TeamViewModel(
 			team: team
+		);
+	}
+}
+
+class _AppBar extends ViewModelWidget<TeamViewModel>
+{
+	@override
+	Widget build(context, model)
+	{
+		return SliverAppBar(
+			title: Text(
+				"${model.team.name}"
+			),
+			floating: true,
+		);
+	}
+}
+
+class _TeamPlayersView extends ViewModelBuilderWidget<TeamPlayersViewModel>
+{
+	@override
+	Widget builder(context, model, child)
+	{
+		return model.dataReady ?
+		SliverList(
+			delegate: SliverChildBuilderDelegate(
+				(context, index) {
+					var player = model.data[index];
+					return Card(
+						child: ListTile(
+							leading: CircleAvatar(
+								child: Icon(
+									Icons.person,
+									size: 32,
+								),
+							),
+							title: Text(
+								player.username
+							),
+						)
+					);
+				},
+				childCount: model.data.length
+			),
+		) :
+		SliverToBoxAdapter(
+			child: Center(
+				child: CircularProgressIndicator(),
+			),
+		);
+	}
+
+	@override
+	TeamPlayersViewModel viewModelBuilder(context)
+	{
+		return TeamPlayersViewModel(
+			context: context
 		);
 	}
 }
