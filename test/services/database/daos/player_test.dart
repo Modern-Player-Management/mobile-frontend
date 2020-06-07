deeace3c3cda84e016904501d341b3f7e7a21cfb
JM REMEUR <jm@remeur.fr>
2020-06-07 20:58:04 +0200

    adde player simple tests

---
+++ test/services/database/daos/player_test.dart
@@ -0,0 +1,91 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:matcher/matcher.dart';
+import 'package:sqflite_ffi_test/sqflite_ffi_test.dart';
+
+import 'package:mpm/services/database/database.dart';
+
+import 'team_test.dart';
+
+String teamId = "team";
+String player = "player";
+
+String playerId = "player";
+String email = "test@test.fr";
+String username = "username";
+
+AppDatabase _db;
+
+void main()
+{
+	TestWidgetsFlutterBinding.ensureInitialized();
+	sqfliteFfiTestInit();
+
+	setUp(() async {
+		_db = await $FloorAppDatabase.inMemoryDatabaseBuilder().build();
+	});
+
+	group('simple database player tests', (){
+		
+		test('insert one player', insert_player);
+		test('find one player', find_player);
+		test('update player', update_player);
+		test('delete player', delete_player);
+	});
+
+	tearDown((){
+		_db.close();
+	});
+}
+
+Future<Player> insert_player([AppDatabase db]) async
+{
+	await insert_team(_db);
+
+	final player = Player(
+		id: playerId,
+		email: email,
+		username: username,
+	);
+
+	int id = await (_db ?? db).playerDao.insertPlayer(player);
+
+	expect(id, equals(1));
+
+	return player;
+}
+
+void find_player() async
+{
+	await insert_player();
+
+	final player = await _db.playerDao.getPlayer(playerId);
+
+	expect(player.id, equals(playerId));
+}
+
+void update_player() async
+{
+	final player = await insert_player();
+
+	player.username = "test";
+	int rows = await _db.playerDao.updatePlayer(player);
+
+	expect(rows, equals(1));
+
+	final p = await _db.playerDao.getPlayer(playerId);
+
+	expect(p.username, equals("test"));
+}
+
+void delete_player() async
+{
+	final player = await insert_player();
+
+	var rows = await _db.playerDao.deletePlayer(player);
+
+	expect(rows, equals(1));
+
+	final p = await _db.playerDao.getPlayer(playerId);
+
+	expect(p, equals(null));
+}
