9390cecdb232281810a89f58ce1f051bd28a0f16
JM REMEUR <jm@remeur.fr>
2020-06-07 15:29:17 +0200

    simple search bar

--- lib/ui/views/player/manage/manage_player_view.dart
+++ lib/ui/views/player/manage/manage_player_view.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 import 'package:stacked/stacked.dart';
+import 'package:flappy_search_bar/flappy_search_bar.dart';
 
 import 'package:mpm/services/database/models/team.dart';
 import 'package:mpm/ui/views/player/manage/manage_player_view_model.dart';
+import 'package:mpm/services/database/models/player.dart';
 import 'package:mpm/ui/widgets/button.dart';
 
 class ManagePlayerView extends ViewModelBuilderWidget<ManagePlayerViewModel>
@@ -21,7 +23,25 @@ class ManagePlayerView extends ViewModelBuilderWidget<ManagePlayerViewModel>
   	Widget builder(context, model, child)
 	{
 		return Scaffold(
-			body: Container(),
+			body: Padding(
+				padding: const EdgeInsets.all(8.0),
+				child: SearchBar<Player>(
+					searchBarPadding: EdgeInsets.only(top: 16),
+					onSearch: model.onSearch,
+					onItemFound: (player, index) {
+						return ListTile(
+							leading: CircleAvatar(
+								child: Icon(
+									Icons.person
+								),
+							),
+							title: Text(
+								player.username
+							),
+						);
+					},
+				),
+			),
 			floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
 			floatingActionButton: Button(
 				child: Text(
--- lib/ui/views/player/manage/manage_player_view_model.dart
+++ lib/ui/views/player/manage/manage_player_view_model.dart
@@ -3,14 +3,37 @@ import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
 import 'package:mpm/app/locator.dart';
+import 'package:mpm/utils/utils.dart';
 
 class ManagePlayerViewModel extends BaseViewModel
 {
 	final Team team;
+	final _playerApi = locator<PlayerApi>();
 
 	ManagePlayerViewModel({
 		@required this.team
 	});
+
+	Future<List<Player>> onSearch(String str) async
+	{
+		if(str.length > minCharacters)
+		{
+			try
+			{
+				var res = await _playerApi.searchPlayers(str);
+				if(res.isSuccessful) 
+				{
+					return res.body;
+				}
+			}
+			catch(e)
+			{
+				print(e);
+			}
+		}
+
+		return [];
+	}
 
 	void managePlayer()
 	{
