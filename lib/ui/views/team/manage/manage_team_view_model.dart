25f8d76b3f7f6955812f5014e742132d4d8bc33b
JM REMEUR <jm@remeur.fr>
2020-06-07 16:12:16 +0200

    upload image in the database

--- lib/ui/views/team/manage/manage_team_view_model.dart
+++ lib/ui/views/team/manage/manage_team_view_model.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:stacked/stacked.dart';
+import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
 
 import 'package:mpm/app/locator.dart';
 import 'package:mpm/utils/utils.dart';
@@ -13,6 +15,8 @@ class ManageTeamViewModel extends BaseViewModel
 {
 	final _storage = locator<SecureStorage>();
 	final _teamManager = locator<TeamManager>();
+	final _fileApi = locator<FileApi>();
+	final _navigation = locator<NavigationService>();
 
 	final formKey = GlobalKey<FormState>();
 
@@ -59,14 +63,41 @@ class ManageTeamViewModel extends BaseViewModel
 		notifyListeners();
 	}
 
-	void manageTeam()
+	void manageTeam() async
 	{
 		if(formKey.currentState.validate())
 		{
 			formKey.currentState.save();
 			team.managerId = _storage.player;
-			
+
+			String fileId;
+
+			if(image != null)
+			{
+				http.MultipartFile file = await http.MultipartFile.fromPath(
+					'file',
+					image.path,
+					contentType: MediaType('image', 'png')
+				);
+				
+				try
+				{
+					var res = await _fileApi.uploadFile(file);
+					if(res.isSuccessful)
+					{
+						fileId = res.body["id"];
+					}
+				}
+				catch(e)
+				{
+					print(e);
+				}
+			}
+
+			team.image = fileId;
 			_teamManager.insertTeam(team);
+
+			_navigation.back();
 		}
 	}
 }
