e06cf7832ef0984bb878d304f3a1dcea60064b85
JM REMEUR <jm@remeur.fr>
2020-06-08 10:58:32 +0200

    moved search function add team player function

--- lib/ui/views/player/search/search_player_view_model.dart
+++ lib/ui/views/player/search/search_player_view_model.dart
@@ -3,40 +3,24 @@ import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
 import 'package:mpm/app/locator.dart';
-import 'package:mpm/utils/utils.dart';
 
 class SearchPlayerViewModel extends BaseViewModel
 {
+	final _playerManager = locator<PlayerManager>();
+
+	final _navigation = locator<NavigationService>();
+
 	final Team team;
-	final _playerApi = locator<PlayerApi>();
 
 	SearchPlayerViewModel({
 		@required this.team
 	});
 
-	Future<List<Player>> onSearch(String str) async
+	Future<List<Player>> onSearch(String str) async => _playerManager.search(str);
+
+	void addPlayer(Player player) async
 	{
-		if(str.length > minCharacters)
-		{
-			try
-			{
-				var res = await _playerApi.searchPlayers(str);
-				if(res.isSuccessful) 
-				{
-					return res.body;
-				}
-			}
-			catch(e)
-			{
-				print(e);
-			}
-		}
-
-		return [];
-	}
-
-	void managePlayer()
-	{
-
+		await _playerManager.addTeamPlayer(team, player);
+		_navigation.back();
 	}
 }
