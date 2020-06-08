44631ff0fcbcd5b939d668f346425603026eba53
JM REMEUR <jm@remeur.fr>
2020-06-08 09:31:41 +0200

    display simple player image

--- lib/ui/views/player/player_view.dart
+++ lib/ui/views/player/player_view.dart
@@ -4,6 +4,7 @@ import 'package:stacked/stacked.dart';
 
 import 'package:mpm/services/database/models/player.dart';
 import 'package:mpm/ui/views/player/player_view_model.dart';
+import 'package:mpm/ui/widgets/circle_avatar_image.dart';
 
 class PlayerView extends ViewModelBuilderWidget<PlayerViewModel>
 {
@@ -25,7 +26,14 @@ class PlayerView extends ViewModelBuilderWidget<PlayerViewModel>
 					"MPM"
 				),
 			),
-			body: Container()
+			body: Padding(
+				padding: const EdgeInsets.all(8.0),
+				child: Column(
+					children: <Widget>[
+						_Header()
+					],
+				),
+			)
 		);
 	}
   
@@ -34,6 +42,24 @@ class PlayerView extends ViewModelBuilderWidget<PlayerViewModel>
 	{
 		return PlayerViewModel(
 			player: player
+		);
+	}
+}
+
+class _Header extends ViewModelWidget<PlayerViewModel>
+{
+	@override
+	Widget build(context, model)
+	{
+		return Row(
+			children: <Widget>[
+				CircleAvatarImage(
+					url: model.url,
+					headers: model.headers,
+					hasImage: model.hasImage,
+					icon: Icons.person,
+				)
+			],
 		);
 	}
 }
--- lib/ui/views/player/player_view_model.dart
+++ lib/ui/views/player/player_view_model.dart
@@ -3,12 +3,26 @@ import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
 import 'package:mpm/app/locator.dart';
+import 'package:mpm/utils/utils.dart';
 
 class PlayerViewModel extends BaseViewModel
 {
+	final _storage = locator<SecureStorage>();
+
 	final Player player;
+
+	String url;
+	Map<String, String> headers;
 
 	PlayerViewModel({
 		@required this.player
-	});
+	})
+	{
+		url = "$serverUrl/api/files/${player.image}";
+		headers = {
+			"Authorization": "Bearer ${_storage.token}"
+		};
+	}
+
+	bool get hasImage => player?.image != null;
 }
