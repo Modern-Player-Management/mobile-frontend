ff0e05830d5b5d41f55d2d1fdeed919338f0e2b0
JM REMEUR <jm@remeur.fr>
2020-06-29 16:04:38 +0200

    simple team widget

---
+++ lib/ui/widgets/team_widget.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'package:mpm/services/database/models/team.dart';
+import 'package:mpm/ui/widgets/circle_avatar_image.dart';
+
+class TeamWidget extends StatelessWidget
+{
+	final Team team;
+	final Function(Team team) onTap;
+
+	TeamWidget({
+		@required this.team,
+		@required this.onTap
+	});
+
+	@override
+	Widget build(context)
+	{
+		return Hero(
+			key: UniqueKey(),
+			tag: team.id,
+			child: Card(
+				child: ListTile(
+					leading: CircleAvatarImage(
+						image: team.image,
+					),
+					title: Text(
+						"${team.name}"
+					),
+					subtitle: Text(
+						"Manager : ${team.manager.username}"
+					),
+					trailing: Text(
+						"Players : ${team.players.length}"
+					),
+					onTap: () => onTap(team),
+				),
+			),
+		);
+	}
+}
