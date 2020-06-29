c173c499d17e96f60761dbbe284ae813364cdb28
JM REMEUR <jm@remeur.fr>
2020-06-30 00:37:07 +0200

    fixed all tests, correct names

--- lib/services/database/daos/team_player_dao.dart
+++ lib/services/database/daos/team_player_dao.dart
@@ -6,18 +6,18 @@ import 'package:mpm/services/database/models/team_player.dart';
 @dao
 abstract class TeamPlayerDao extends ModelDao<TeamPlayer>
 {
-	@Query('select * from team_players where teamId = :teamId and `delete` = 0')
+	@Query('select * from team_players where teamId = :teamId and deleted = 0')
 	Stream<List<TeamPlayer>> getTeamPlayers(String teamId);
 
-	@Query('select * from team_players where teamId = :teamId and `delete` = 0')
+	@Query('select * from team_players where teamId = :teamId and deleted = 0')
 	Future<List<TeamPlayer>> getAllTeamPlayers(String teamId);
 
-	@Query('select * from team_players where teamId = :teamId and save = 1 and `delete` = 0')
+	@Query('select * from team_players where teamId = :teamId and saved = 1 and deleted = 0')
 	Future<List<TeamPlayer>> getSavedTeamPlayers(String teamId);
 
-	@Query('select * from team_players where teamId = :teamId and save = 0 and `delete` = 0')
+	@Query('select * from team_players where teamId = :teamId and saved = 0 and deleted = 0')
 	Future<List<TeamPlayer>> getUnsavedTeamPlayers(String teamId);
 
-	@Query('select * from team_players where teamId = :teamId and `delete` = 1')
+	@Query('select * from team_players where teamId = :teamId and deleted = 1')
 	Future<List<TeamPlayer>> getUndeletedTeamPlayers(String teamId);
 }
