dcb49d8eb1dc9959c340a7239666b732a835057d
JM REMEUR <jm@remeur.fr>
2020-06-29 18:55:49 +0200

    better queries

--- lib/services/database/daos/game_dao.dart
+++ lib/services/database/daos/game_dao.dart
@@ -6,15 +6,17 @@ import 'package:mpm/services/database/models/game.dart';
 @dao
 abstract class GameDao extends ModelDao<Game>
 {
-	@Query('select * from events where team = :team and `delete` = 0')
-	Stream<List<Game>> getGames(String team);
+	@Query('select * from events where id = :id')
+	Future<Game> getGame(String id);
 
-	@Query('select * from events where team = :team and save = 1 and `delete` = 0')
-	Future<List<Game>> getSavedGames(String team);
+	@Query('update games set id = :newId where id = :oldId')
+	Future<void> updateGameId(String oldId, String newId);
 
-	@Query('select * from events where team = :team and save = 0 and `delete` = 0')
-	Future<List<Game>> getUnsavedGames(String team);
+	@Query('select * from games inner join team_games on games.id = team_games.gameId '
+		'and team_games.teamId = :teamId and `team_games.delete` = 0')
+	Stream<List<Game>> getGames(String teamId);
 
-	@Query('select * from events where team = :team and `delete` = 1')
-	Future<List<Game>> getUndeletedGames(String team);
+	@Query('select * from games inner join team_games on games.id = team_games.gameId '
+		'and team_games.teamId = :teamId and `team_games.delete` = 0')
+	Future<List<Game>> getAllGames(String teamId);
 }
