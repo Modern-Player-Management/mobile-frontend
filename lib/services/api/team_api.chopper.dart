c57114989de871b8d1b88c33640b811472b9a3bd
JM REMEUR <jm@remeur.fr>
2020-07-01 23:12:16 +0200

    rebuild files

--- lib/services/api/team_api.chopper.dart
+++ lib/services/api/team_api.chopper.dart
@@ -18,7 +18,7 @@ class _$TeamApi extends TeamApi {
 
   @override
   Future<Response<Team>> createTeam(Team team) {
-    final $url = '/api/teams';
+    final $url = '/';
     final $body = team;
     final $request = Request('POST', $url, client.baseUrl, body: $body);
     return client.send<Team, Team>($request);
@@ -26,28 +26,21 @@ class _$TeamApi extends TeamApi {
 
   @override
   Future<Response<List<Team>>> getTeams() {
-    final $url = '/api/teams';
+    final $url = '/';
     final $request = Request('GET', $url, client.baseUrl);
     return client.send<List<Team>, Team>($request);
   }
 
   @override
-  Future<Response<dynamic>> addTeamPlayer(String teamId, String playerId) {
-    final $url = '/api/teams/$teamId/player/$playerId';
-    final $request = Request('POST', $url, client.baseUrl);
-    return client.send<dynamic, dynamic>($request);
-  }
-
-  @override
-  Future<Response<dynamic>> deleteTeamPlayer(String teamId, String playerId) {
-    final $url = '/api/teams/$teamId/player/$playerId';
-    final $request = Request('DELETE', $url, client.baseUrl);
-    return client.send<dynamic, dynamic>($request);
+  Future<Response<Team>> getTeam() {
+    final $url = '/{teamId}';
+    final $request = Request('GET', $url, client.baseUrl);
+    return client.send<Team, Team>($request);
   }
 
   @override
   Future<Response<dynamic>> updateTeam(String teamId, Team team) {
-    final $url = '/api/teams/$teamId';
+    final $url = '/$teamId';
     final $body = team;
     final $request = Request('PUT', $url, client.baseUrl, body: $body);
     return client.send<dynamic, dynamic>($request);
@@ -55,14 +48,28 @@ class _$TeamApi extends TeamApi {
 
   @override
   Future<Response<dynamic>> deleteTeam(String teamId) {
-    final $url = '/api/teams/$teamId';
+    final $url = '/$teamId';
+    final $request = Request('DELETE', $url, client.baseUrl);
+    return client.send<dynamic, dynamic>($request);
+  }
+
+  @override
+  Future<Response<dynamic>> addTeamPlayer(String teamId, String playerId) {
+    final $url = '/$teamId/player/$playerId';
+    final $request = Request('POST', $url, client.baseUrl);
+    return client.send<dynamic, dynamic>($request);
+  }
+
+  @override
+  Future<Response<dynamic>> deleteTeamPlayer(String teamId, String playerId) {
+    final $url = '/$teamId/player/$playerId';
     final $request = Request('DELETE', $url, client.baseUrl);
     return client.send<dynamic, dynamic>($request);
   }
 
   @override
   Future<Response<Event>> addEvent(String teamId, Event event) {
-    final $url = '/api/teams/$teamId/events';
+    final $url = '/$teamId/events';
     final $body = event;
     final $request = Request('POST', $url, client.baseUrl, body: $body);
     return client.send<Event, Event>($request);
@@ -70,8 +77,15 @@ class _$TeamApi extends TeamApi {
 
   @override
   Future<Response<Event>> addGame(String teamId, MultipartFile file) {
-    final $url = '/api/teams/$teamId/games';
+    final $url = '/$teamId/games';
     final $request = Request('POST', $url, client.baseUrl);
     return client.send<Event, Event>($request);
+  }
+
+  @override
+  Future<Response<PlayerStats>> getPlayerStats() {
+    final $url = '/{teamId}/stats';
+    final $request = Request('GET', $url, client.baseUrl);
+    return client.send<PlayerStats, PlayerStats>($request);
   }
 }
