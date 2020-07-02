10999fb236da7382d3bd9d93b6af17c550c76dc0
JM REMEUR <jm@remeur.fr>
2020-07-02 23:30:42 +0200

    update daos with all required queries

--- lib/services/database/daos/event_discrepancy_dao.dart
+++ lib/services/database/daos/event_discrepancy_dao.dart
@@ -6,6 +6,12 @@ import 'package:mpm/services/database/models/event_discrepancy.dart';
 @dao
 abstract class EventDiscrepancyDao extends ModelDao<EventDiscrepancy>
 {
+	@Query('select * from events_discrepancies where eventId = :eventId and deleted = 0')
+	Stream<List<EventDiscrepancy>> getEventDiscrepancies(String eventId);
+
+	@Query('select * from events_discrepancies where eventId = :eventId and deleted = 0')
+	Future<List<EventDiscrepancy>> getAllEventDiscrepancies(String eventId);
+
 	@Query('select * from events_discrepancies where eventId = :eventId and saved = 1 and `deleted` = 0')
 	Future<List<EventDiscrepancy>> getSavedEventDiscrepancies(String eventId);
 
