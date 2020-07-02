98d6cd7fdd8e35d9db82e75032ddb213b61917cb
JM REMEUR <jm@remeur.fr>
2020-07-02 23:30:58 +0200

    update database models and daos

--- lib/services/database/database.dart
+++ lib/services/database/database.dart
@@ -16,12 +16,14 @@ part 'database.g.dart';
 	Team,
 	Player,
 	TeamPlayer,
-	Game,
-	TeamGame,
 	Event,
 	TeamEvent,
 	Discrepancy,
 	EventDiscrepancy,
+	Participation,
+	EventParticipation,
+	Game,
+	TeamGame
 ], version: 1)
 @singleton
 abstract class AppDatabase extends FloorDatabase 
@@ -29,12 +31,14 @@ abstract class AppDatabase extends FloorDatabase
 	TeamDao get teamDao;
 	PlayerDao get playerDao;
 	TeamPlayerDao get teamPlayerDao;
-	GameDao get gameDao;
-	TeamGameDao get teamGameDao;
 	EventDao get eventDao;
 	TeamEventDao get teamEventDao;
 	DiscrepancyDao get discrepancyDao;
 	EventDiscrepancyDao get eventDiscrepancyDao;
+	ParticipationDao get participationDao;
+	EventParticipationDao get eventParticipationDao;
+	GameDao get gameDao;
+	TeamGameDao get teamGameDao;
 
 	@factoryMethod
 	static Future<AppDatabase> create() async 
