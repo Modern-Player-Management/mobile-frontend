743974fb777f363713377e0f10604fe713503b58
JM REMEUR <jm@remeur.fr>
2020-07-13 20:57:06 +0200

    create team players

---
+++ lib/ui/views/team/player/team_players_view.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import 'package:stacked/stacked.dart';
+
+import 'package:mpm/ui/views/team/player/team_players_view_model.dart';
+import 'package:mpm/ui/widgets/circle_avatar_image.dart';
+
+class TeamPlayersView extends ViewModelBuilderWidget<TeamPlayersViewModel>
+{
+	@override
+	Widget builder(context, model, child)
+	{
+		return model.dataReady ?
+		ListView.builder(
+			physics: BouncingScrollPhysics(),
+			itemCount: model.data.length,
+			itemBuilder: (context, index) {
+				var player = model.data[index];
+				return Card(
+					child: ListTile(
+						leading: CircleAvatarImage(
+							image: player.image,
+							icon: Icons.person,
+						),
+						title: Text(
+							player.username
+						),
+						trailing: model.isManager ?
+						IconButton(
+							icon: Icon(
+								Icons.delete,
+								color: Colors.red,
+							),
+							onPressed: () => model.onPressed(player),
+						) : null,
+						onTap: () => model.onTap(player),
+					)
+				);
+			}
+		) :
+		Center(
+			child: CircularProgressIndicator(),
+		);
+	}
+
+	@override
+	TeamPlayersViewModel viewModelBuilder(context)
+	{
+		return TeamPlayersViewModel(
+			context: context
+		);
+	}
+}
---
+++ lib/ui/views/team/player/team_players_view_model.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'package:stacked/stacked.dart';
+
+import 'package:mpm/app/locator.dart';
+import 'package:mpm/ui/views/team/team_view_model.dart';
+
+class TeamPlayersViewModel extends StreamViewModel<List<Player>>
+{
+	final _playerManager = locator<PlayerManager>();
+
+	final _playerDao = locator<AppDatabase>().playerDao;
+	final _navigation = locator<NavigationService>();
+
+	final BuildContext context;
+
+	TeamViewModel _teamViewModel;
+
+	TeamPlayersViewModel({
+		@required this.context
+	})
+	{
+		_teamViewModel = getParentViewModel<TeamViewModel>(context);
+	}
+
+	@override
+	get stream async * {
+		await for(var players in _playerDao.getStream(_teamViewModel.team.id, _teamViewModel.team.managerId))
+		{
+			_teamViewModel.setPlayers(players);
+			yield players;
+		}
+	}
+
+	void onPressed(Player player) async
+	{
+		final teamPlayer = TeamPlayer(
+			teamId: _teamViewModel.team.id,
+			playerId: player.id
+		);
+
+		await _playerManager.removeTeamPlayer(teamPlayer);
+	}
+
+	void onTap(Player player)
+	{
+		_navigation.navigateTo(
+			Routes.playerView,
+			arguments: PlayerViewArguments(
+				player: player
+			)
+		);
+	}
+
+	bool get isManager => _teamViewModel.isManager;
+}
