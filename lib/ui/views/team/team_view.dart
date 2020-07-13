7938dd912beca628b35d28578a3392915f799c2a
JM REMEUR <jm@remeur.fr>
2020-07-13 20:58:29 +0200

    simplify team view added team games

--- lib/ui/views/team/team_view.dart
+++ lib/ui/views/team/team_view.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 import 'package:stacked/stacked.dart';
-import 'package:table_calendar/table_calendar.dart';
 import 'package:ff_navigation_bar/ff_navigation_bar.dart';
 
-import 'package:mpm/services/database/models/event.dart';
 import 'package:mpm/services/database/models/team.dart';
 import 'package:mpm/ui/views/team/team_view_model.dart';
+import 'package:mpm/ui/views/team/event/team_events_view.dart';
+import 'package:mpm/ui/views/team/player/team_players_view.dart';
+import 'package:mpm/ui/views/team/game/team_games_view.dart';
 import 'package:mpm/ui/widgets/circle_avatar_image.dart';
-import 'package:mpm/utils/colors.dart';
 
 class TeamView extends ViewModelBuilderWidget<TeamViewModel>
 {
@@ -36,7 +36,7 @@ class TeamView extends ViewModelBuilderWidget<TeamViewModel>
 				],
 			),
 			bottomNavigationBar: _NavigationBar(),
-			floatingActionButton: model.isManager ?
+			floatingActionButton: model.isManager && !model.isGameTab ?
 			FloatingActionButton(
 				child: Icon(
 					Icons.add
@@ -53,7 +53,33 @@ class TeamView extends ViewModelBuilderWidget<TeamViewModel>
 			team: team
 		);
 	}
-  
+}
+
+class _NavigationBar extends ViewModelWidget<TeamViewModel>
+{
+	@override
+	Widget build(context, model)
+	{
+		return FFNavigationBar(
+			theme: FFNavigationBarTheme(),
+			selectedIndex: model.selectedTab,
+			onSelectTab: model.onSelectTab,
+			items: [
+				FFNavigationBarItem(
+					iconData: Icons.calendar_today,
+					label: 'Events',
+				),
+				FFNavigationBarItem(
+					iconData: Icons.group,
+					label: 'Players',
+				),
+				FFNavigationBarItem(
+					iconData: Icons.games,
+					label: 'Games',
+				)
+			],
+		);
+	}
 }
 
 class _Header extends ViewModelWidget<TeamViewModel>
@@ -106,177 +132,10 @@ class _Tabs extends ViewModelWidget<TeamViewModel>
 			controller: model.controller,
 			onPageChanged: model.onPageChanged,
 			children: <Widget>[
-				_Events(),
-				_TeamPlayersView()
+				TeamEventsView(),
+				TeamPlayersView(),
+				TeamGamesView()
 			],
-		);
-	}
-}
-
-class _NavigationBar extends ViewModelWidget<TeamViewModel>
-{
-	@override
-	Widget build(context, model)
-	{
-		return FFNavigationBar(
-			theme: FFNavigationBarTheme(),
-			selectedIndex: model.selectedTab,
-			onSelectTab: model.onSelectTab,
-			items: [
-				FFNavigationBarItem(
-					iconData: Icons.calendar_today,
-					label: 'Events',
-				),
-				FFNavigationBarItem(
-					iconData: Icons.group,
-					label: 'Players',
-				)
-			],
-		);
-	}
-}
-
-class _Events extends ViewModelBuilderWidget<TeamCalendarViewModel>
-{
-	@override
-	Widget builder(context, model, child)
-	{
-		return CustomScrollView(
-			physics: BouncingScrollPhysics(),
-			slivers: <Widget>[
-				SliverToBoxAdapter(
-					child: _Calendar(),
-				),
-				SliverToBoxAdapter(
-					child: Padding(
-						padding: const EdgeInsets.symmetric(horizontal: 8),
-						child: Text(
-							"Events",
-							style: Theme.of(context).textTheme.headline6,
-						),
-					),
-				),
-				SliverList(
-					delegate: SliverChildBuilderDelegate(
-						(context, index) {
-							Event event = model.selectedEvents[index];
-							return ListTile(
-								title: Text(
-									event.name
-								),
-							);
-						},
-						childCount: model.selectedEvents.length
-					),
-				)
-			],	
-		);
-	}
-
-	@override
-	TeamCalendarViewModel viewModelBuilder(context)
-	{
-		return TeamCalendarViewModel(
-			context: context
-		);
-	}
-}
-
-class _Calendar extends ViewModelWidget<TeamCalendarViewModel>
-{
-	@override
-	Widget build(context, model)
-	{
-		return TableCalendar(
-			locale: 'fr_FR',
-			headerStyle: HeaderStyle(
-				formatButtonVisible: false
-			),
-			calendarController: model.calendarController,
-			onDaySelected: model.onDaySelected,
-			events: model.data,
-			calendarStyle: CalendarStyle(
-				outsideDaysVisible: false,
-				todayColor: ThemeColors.accent,
-				selectedColor: ThemeColors.primary
-			),
-			availableGestures: AvailableGestures.horizontalSwipe,
-			builders: CalendarBuilders(
-				markersBuilder: (context, date, events, holidays) {
-					if(events.isNotEmpty)
-					{
-						return [AnimatedContainer(
-							duration: const Duration(milliseconds: 300),
-							decoration: BoxDecoration(
-								shape: BoxShape.rectangle,
-								color: model.calendarController.isToday(date) ? 
-									ThemeColors.primary :
-									ThemeColors.accent
-							),
-							width: 16.0,
-							height: 16.0,
-							child: Center(
-								child: Text(
-									'${events.length}',
-									style: TextStyle().copyWith(
-										color: Colors.white,
-										fontSize: 12,
-									),
-								),
-							),
-						)];
-					}
-
-					return [];
-				}
-			),
-		);
-	}
-}
-
-class _TeamPlayersView extends ViewModelBuilderWidget<TeamPlayersViewModel>
-{
-	@override
-	Widget builder(context, model, child)
-	{
-		return model.dataReady ?
-		ListView.builder(
-			physics: BouncingScrollPhysics(),
-			itemCount: model.data.length,
-			itemBuilder: (context, index) {
-				var player = model.data[index];
-				return Card(
-					child: ListTile(
-						leading: CircleAvatarImage(
-							image: player.image,
-							icon: Icons.person,
-						),
-						title: Text(
-							player.username
-						),
-						trailing: model.isManager ?
-						IconButton(
-							icon: Icon(
-								Icons.delete,
-								color: Colors.red,
-							),
-							onPressed: () => model.onPressed(player),
-						) : null,
-						onTap: () => model.onTap(player),
-					)
-				);
-			}
-		) :
-		Center(
-			child: CircularProgressIndicator(),
-		);
-	}
-
-	@override
-	TeamPlayersViewModel viewModelBuilder(context)
-	{
-		return TeamPlayersViewModel(
-			context: context
 		);
 	}
 }
